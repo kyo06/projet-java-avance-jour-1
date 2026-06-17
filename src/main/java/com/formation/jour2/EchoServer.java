181573154fe1db15dca73579530e59d7cae20c10
Mohammed REZGUI <m.rezgui06@gmail.com>
2026-06-17 12:01:33 +0200

    feat: add Executor for EchoServer

--- src/main/java/com/formation/jour2/EchoServer.java
+++ src/main/java/com/formation/jour2/EchoServer.java
@@ -2,20 +2,27 @@ package com.formation.jour2;
 
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EchoServer {
     public static void main(String[] args) {
         int port = 12345;
+
+        ExecutorService pool = Executors.newCachedThreadPool();
+        int nbClientsConnectes = 0;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Serveur démarré sur le port " + port);
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
+                System.out.println("Nombre de clients connectés : " + ++nbClientsConnectes);
+
                 System.out.println("Client connecté : "
                         + clientSocket.getInetAddress());
 
-                new Thread(() -> handleClient(clientSocket)).start();
+                pool.submit(() -> handleClient(clientSocket));
             }
         } catch (IOException e) {
             e.printStackTrace();
